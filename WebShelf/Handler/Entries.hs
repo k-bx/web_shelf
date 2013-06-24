82012935df9b3594bbf11a06cd3895a7c3535fcf
Konstantine Rybnikov <k-bx@k-bx.com>
2013-06-25 00:33:26 +0300

    Add Entries resource

---
+++ WebShelf/Handler/Entries.hs
@@ -0,0 +1,15 @@
+module Handler.Entries where
+
+import Import
+
+getEntriesR :: Handler Html
+getEntriesR = error "Not yet implemented: getEntriesR"
+
+postEntriesR :: Handler Html
+postEntriesR = error "Not yet implemented: postEntriesR"
+
+putEntriesR :: Handler Html
+putEntriesR = error "Not yet implemented: putEntriesR"
+
+deleteEntriesR :: Handler Html
+deleteEntriesR = error "Not yet implemented: deleteEntriesR"
